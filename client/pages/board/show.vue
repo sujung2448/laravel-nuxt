f0877b9cf69e8c078f6ee45e0a9f8ab9c4cd1c0c
sujung2448 <sujung2448@naver.com>
2022-04-20 11:53:02 +0900

    first

---
+++ client/pages/board/show.vue
@@ -0,0 +1,20 @@
+<template>
+  <card>
+    <div class="border-b border-gray-300 mb-8 pl-1 pb-2 text-xl font-bold">
+      {{}}
+    </div>
+    <div class="text-lg">
+      {{}}
+    </div>
+  </card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
